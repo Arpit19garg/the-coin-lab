798b7098643cd474aec10a89afbcc7fb7562fa67
Arpit Garg <gargarpit2002@gmail.com>
2025-04-21 12:07:34 +0530

    Update index.js again with BrowserRouter and basename

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
 root.render(
-  <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter basename="/the-coin-lab">
+    <App />
+  </BrowserRouter>
 );
