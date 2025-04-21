38868e98044faa05bd8e9ab7203d6a7785c18b6d
Arpit Garg <gargarpit2002@gmail.com>
2025-04-21 21:32:30 +0530

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -3,14 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
 
 // Wrap your App with BrowserRouter
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter basename="/the-coin-lab"> {/* Use your repo name here */}
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+root.render(<App />);
